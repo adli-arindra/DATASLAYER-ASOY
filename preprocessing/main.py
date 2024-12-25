02d3b5cd1b7bb5649c40cfce84cdd827154e57ac
Adli Arindra <adliarindra27@gmail.com>
2024-12-25 10:56:09 +0700

    nambahin processed)test

--- preprocessing/main.py
+++ preprocessing/main.py
@@ -17,7 +17,7 @@ def process_image(input_path: str, output_path: str):
     masked_scaled = masked_image * 255
     cv2.imwrite(output_path, masked_scaled)
 
-if __name__ == "__main__":
+def process_renamed():
     input_path = "renamed/"
     output_path = "processed/"
     for file in os.listdir(input_path):
@@ -26,3 +26,17 @@ if __name__ == "__main__":
         print(f"Successfully masked {file}")
 
     print("All done")
+
+
+def process_test():
+    input_path = "test/"
+    output_path = "processed_test/"
+    for file in os.listdir(input_path):
+        process_image(os.path.join(input_path, file),
+                      os.path.join(output_path, file))
+        print(f"Successfully masked {file}")
+    print("Done")
+
+
+if __name__ == "__main__":
+    process_test()
